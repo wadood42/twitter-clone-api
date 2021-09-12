2adecee14b0e38df6c29c4103950d40a87981ebb
Abdul <wbehzad12@gmail.com>
2021-09-12 10:13:20 -0500

    no nodemone

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const nodemon = require("nodemon");
+// const nodemon = require("nodemon");
 const app = express();
 const authRoutes = require("./routes/auth");
 require("dotenv").config({ path: "./configs/.env" });
