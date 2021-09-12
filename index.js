7d500e5ccaed4d9541d566f870a576b266cb5e79
Abdul <wbehzad12@gmail.com>
2021-09-12 10:05:26 -0500

    testing

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-const express = require("express")
+const express = require("express");
 const mongoose = require("mongoose");
 const nodemon = require("nodemon");
-const app = express()
-const authRoutes = require("./routes/auth")
+const app = express();
+const authRoutes = require("./routes/auth");
 require("dotenv").config({ path: "./configs/.env" });
 
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080
-
-mongoose.connect(process.env.MONGO_DB_URI, {
+mongoose
+  .connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log("CONNECT TO MONGODB")
-}).then(() => {
+  })
+  .then(() => {
+    console.log("CONNECT TO MONGODB");
+  })
+  .then(() => {
     app.listen(PORT, () => {
-        console.log("LISTENING ON PORT ", PORT)
-    })
-})
-.catch((err) => {
-    console.log("ERROR CONNECTING TO MONGODB", err)
-})
+      console.log("LISTENING ON PORT ", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("ERROR CONNECTING TO MONGODB", err);
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // ROUTES
 
-app.use("/", authRoutes)
+app.use("/hello", (req, res) => {
+  res.send("Hello");
+});
+
+app.use("/", authRoutes);
 
 app.use("/hello", (req, res) => {
-    res.send("Welcome Abdul")
-})
-
-
+  res.send("Welcome Abdul");
+});
